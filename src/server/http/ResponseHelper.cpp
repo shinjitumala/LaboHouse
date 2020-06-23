9a18517c6e32b145f742b040aa2e537c12a20132
shinjitumala <stumala2797@dune.ocn.ne.jp>
2020-06-23 15:58:54 +0900

    Implement Helpers for HTTP response.

---
+++ src/server/http/ResponseHelper.cpp
@@ -0,0 +1,33 @@
+/// @file ResponseHelper.cpp
+/// @author FPR (funny.pig.run __ ATMARK__ gmail.com)
+///
+/// @copyright Copyright (c) 2020
+///
+/// Part of the LaboHouse tool. Proprietary and confidential.
+/// See the licenses directory for details.
+#include <labo/server/http/Response.h>
+#include <labo/server/http/ResponseHelper.h>
+
+namespace labo::http {
+Response
+not_found(string error)
+{
+    return { Response::Status::NOT_FOUND, { { "error", error } } };
+};
+
+Response
+unauthorized(string error)
+{
+    return { Response::Status::NOT_FOUND, { { "error", error } } };
+};
+
+Response
+forbidden(string error){
+    return { Response::Status::FORBIDDEN, { { "error", error } } };
+}
+
+Response
+bad_request(string error){
+    return { Response::Status::BAD_REQUEST, { { "error", error } } };
+}
+};
