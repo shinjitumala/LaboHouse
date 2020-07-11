665b4a61b12e0c671a7680dd368b894e89bdb67a
shinjitumala <stumala2797@dune.ocn.ne.jp>
2020-07-11 19:21:07 +0900

    Implemented rand.

---
+++ include/labo/util/rand.h
@@ -0,0 +1,16 @@
+/// @file rand.h
+/// @author FPR (funny.pig.run __ ATMARK__ gmail.com)
+///
+/// @copyright Copyright (c) 2020
+///
+/// Part of the SWORD tool. Proprietary and confidential.
+/// See the licenses directory for details.
+#pragma once
+
+#include <string>
+
+namespace labo {
+using namespace std;
+string
+random_string(size_t len);
+};
---
+++ src/util/rand.cpp
@@ -0,0 +1,23 @@
+/// @file rand.cpp
+/// @author FPR (funny.pig.run __ ATMARK__ gmail.com)
+///
+/// @copyright Copyright (c) 2020
+///
+/// Part of the SWORD tool. Proprietary and confidential.
+/// See the licenses directory for details.
+#include <functional>
+#include <labo/util/rand.h>
+#include <random>
+namespace labo {
+mt19937 rand(::time(0));
+auto char_rand = bind(uniform_int_distribution<char>(), rand);
+
+string random_string(size_t len){
+    string s;
+    s.resize(len);
+    for(auto &c : s){
+        c = char_rand();
+    }
+    return s;
+}
+};
